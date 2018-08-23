048e1c3748f5f3c28c56303e3bf0135586dc3743
Robert Fowler <robertf@ipay.co.za>
2018-08-23 14:26:24 +0200

    Update gitignore. Initilise project. Closes #1

---
+++ springboot5-jokes-app-web/src/main/java/com/zenhog/springboot5jokesapp/Springboot5JokesAppApplication.java
@@ -0,0 +1,12 @@
+package com.zenhog.springboot5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot5JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot5JokesAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zenhog/springboot5jokesapp/Springboot5JokesAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zenhog.springboot5jokesapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot5JokesAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
