4c5c09b608457d61d51defb3a1c41edf795943a2
Robert Fowler <robertf@ipay.co.za>
2018-08-23 15:43:05 +0200

    Rename project.

--- sfg-jokes-app-web/src/main/java/com/zenhog/sfgjokesapp/SfgJokesAppApplication.java
+++ sfg-jokes-app-web/src/main/java/com/zenhog/sfgjokesapp/SfgJokesAppApplication.java
@@ -1,12 +1,12 @@
-package com.zenhog.springboot5jokesapp;
+package com.zenhog.sfgjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot5JokesAppApplication {
+public class SfgJokesAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot5JokesAppApplication.class, args);
+        SpringApplication.run(SfgJokesAppApplication.class, args);
     }
 }
--- sfg-jokes-app-web/src/test/java/com/zenhog/springboot5jokesapp/SfgJokesAppApplicationTests.java
+++ sfg-jokes-app-web/src/test/java/com/zenhog/springboot5jokesapp/SfgJokesAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zenhog.springboot5jokesapp;
+package com.zenhog.sfgjokesapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Springboot5JokesAppApplicationTests {
+public class SfgJokesAppApplicationTests {
 
     @Test
     public void contextLoads() {
