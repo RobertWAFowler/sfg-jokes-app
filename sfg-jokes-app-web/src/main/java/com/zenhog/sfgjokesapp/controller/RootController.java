aa71ed36b6f90a10b54d1695c357f9a6008be2c4
Robert Fowler <robertf@ipay.co.za>
2018-08-23 16:49:51 +0200

    Map context root to Jokes view. Closes #5

---
+++ sfg-jokes-app-web/src/main/java/com/zenhog/sfgjokesapp/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.zenhog.sfgjokesapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+    @RequestMapping({"/",""})
+    public String getRoot(){
+        return "jokes";
+    }
+}
